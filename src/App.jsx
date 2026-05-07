ee038b254cb280022b1ac58435dc60bf404e7bf4
zaidrauf7 <iamzaidrauf@gmail.com>
2026-05-07 17:37:47 +0500

    add section1 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Section1 from './components/Section1'
 
 const App = () => {
   return (
     <>
     <Navbar/>
+    <Section1/>
     <div>App</div>
     </>
   )
