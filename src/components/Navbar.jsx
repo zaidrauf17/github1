a185fbbaeffd0b70a3c08e87a464b157a23edab2
zaidrauf7 <iamzaidrauf@gmail.com>
2026-05-07 17:30:44 +0500

    change in app and add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <nav className='w-full h-20 bg-gray-800 text-white flex items-center justify-center'>
+        <h1>Navbar</h1>
+   </nav>
+  )
+}
+
+export default Navbar
